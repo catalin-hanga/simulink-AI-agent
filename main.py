03332e7506c3d46b7ede71d9e56a8945c587246e
Catalin Hanga <catalin.hanga@protonmail.ch>
2025-07-21 10:47:02 +0200

    env variables

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-# from dotenv import load_dotenv, find_dotenv
-# load_dotenv(find_dotenv())
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
 
 import matlab.engine
 eng = matlab.engine.start_matlab()
